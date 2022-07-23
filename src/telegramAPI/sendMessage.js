8dd9363a9913cf144cd6512e6fd96e6bf7719c41
Alisson-J-Silveira <alisson.julio.silveira@outlook.com>
2022-07-22 22:05:51 -0300

    Ajustes na resposta do bot

--- src/telegramAPI/sendMessage.js
+++ src/telegramAPI/sendMessage.js
@@ -4,13 +4,23 @@ const TelegramBot = require('node-telegram-bot-api');
 require('dotenv/config');
 
 const token = process.env.TOKEN;
-const chatIdEnv = process.env.ID_CHAT;
+const chatIdEnvAli = process.env.ID_CHAT_ALI;
+const chatIdEnvRog = process.env.ID_CHAT_ROG;
 
 const bot = new TelegramBot(token, { polling: true });
 
 async function messageSet(lestedCoin) {
     const message = await lestedCoin.href;
 
-    await bot.sendMessage(chatIdEnv, message);
+    await bot.sendMessage(chatIdEnvAli, message);
+    await bot.sendMessage(chatIdEnvRog, message);
 }
+
 module.exports.messageSet = messageSet
+
+bot.on('message', (msg) => {
+    const chatID = msg.chat.id;
+    const firstName = msg.chat.first_name;
+
+    bot.sendMessage(chatID, `Olá ${firstName}, atualmente esse bot está em fase de testes, por isso você não receberá os avisos de lançamento.`);
+});
